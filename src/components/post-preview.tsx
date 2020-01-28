8df434208489a390e01e99743a59e39d735dad27
Pavel Keyzik <pavel.keyzik@gmail.com>
2020-01-28 17:51:18 +0300

    Added animation on hover by post preview

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -22,6 +22,19 @@ const ArticleTitle = styled.h3`
   margin-bottom: 1.5rem;
 `;
 
+const PostImage = styled(Image)`
+  transition: transform 0.2s;
+  transform: scale(1.02);
+
+  :hover {
+    transform: scale(1);
+  }
+`;
+
+const PostImageSection = styled.div`
+  overflow: hidden;
+`;
+
 interface IPostPreview {
   post: IPost;
 }
@@ -30,7 +43,9 @@ function PostPreview({ post }: IPostPreview) {
   return (
     <Article>
       <Link to={`/news/${post.slug}`}>
-        <Image fluid={post.image.sharp.fluid} alt={post.title} fadeIn />
+        <PostImageSection>
+          <PostImage fluid={post.image.sharp.fluid} alt={post.title} fadeIn />
+        </PostImageSection>
       </Link>
       <ArticleContent>
         <ArticleTitle>
