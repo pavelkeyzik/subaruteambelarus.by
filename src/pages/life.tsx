ab8c7ad31a251ab61a69a621811ee71fafa7b5ec
Pavel Keyzik <pavel.keyzik@gmail.com>
2020-02-04 18:55:14 +0300

    Changed width of content

--- src/pages/life.tsx
+++ src/pages/life.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import LayoutWidth from '../components/layouts/layout-width';
 import { css } from '@emotion/core';
 import { events, IEventSection, IEvent } from './life/events';
 import { EventLabel } from './life/event-label';
+import ArticleLayout from '../components/layouts/article';
+import { layoutConfig } from '../components/layouts/config';
+import styled from '@emotion/styled';
 
 const mainCSS = css`
   ul {
@@ -15,34 +17,46 @@ const mainCSS = css`
   }
 `;
 
+const ArticleContent = styled.div`
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 0 16px;
+
+  @media screen and (min-width: ${layoutConfig.small.width}) {
+    padding: 0 40px;
+  }
+`;
+
 function LifePage() {
   return (
-    <LayoutWidth>
-      <h2>Линия жизни</h2>
-      <main css={mainCSS}>
-        {events.map((event: IEventSection, index: number) => (
-          <section key={index}>
-            <h3>{event.headerTitle}</h3>
-            <ul>
-              {event.data.map((eventData: IEvent, eventDataIndex: number) => (
-                <li key={eventDataIndex}>
-                  <EventLabel type={eventData.type} />
-                  <span>
-                    {eventData.link ? (
-                      <a href={eventData.link} target="__blank">
-                        {eventData.title}
-                      </a>
-                    ) : (
-                      eventData.title
-                    )}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </section>
-        ))}
-      </main>
-    </LayoutWidth>
+    <ArticleLayout>
+      <ArticleContent>
+        <h2>Линия жизни</h2>
+        <main css={mainCSS}>
+          {events.map((event: IEventSection, index: number) => (
+            <section key={index}>
+              <h3>{event.headerTitle}</h3>
+              <ul>
+                {event.data.map((eventData: IEvent, eventDataIndex: number) => (
+                  <li key={eventDataIndex}>
+                    <EventLabel type={eventData.type} />
+                    <span>
+                      {eventData.link ? (
+                        <a href={eventData.link} target="__blank">
+                          {eventData.title}
+                        </a>
+                      ) : (
+                        eventData.title
+                      )}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          ))}
+        </main>
+      </ArticleContent>
+    </ArticleLayout>
   );
 }
 
