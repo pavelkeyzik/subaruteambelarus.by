239076485bb24ad98efe2fad3f15ca4e9235c640
Pavel Keyzik <pavel.keyzik@gmail.com>
2020-03-06 09:27:22 +0300

    Added Google Tag Manager

--- src/html.js
+++ src/html.js
@@ -10,6 +10,17 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKXHP3C');
+          `,
+          }}
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} className="light">
